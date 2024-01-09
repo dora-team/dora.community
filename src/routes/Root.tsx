5ba9c1bc720d68d572f4906a9d8c8d41feabaeec
Amanda Lewis <amandaannelewis@gmail.com>
2024-01-09 17:16:00 -0500

    Add Google Analytics Tracking (#52)

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,8 +1,19 @@
 import { Header } from "../components/Header";
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const TRACKING_ID = "UA-243482383-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 export const Root = () => {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+
   return (
     <>
       <Header />
