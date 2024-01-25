091b0450768d375e88426263a527aed6a47aa0d5
Amanda <amandaannelewis@gmail.com>
2024-01-25 16:47:14 +0000

    add January 11th event blog

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,5 +1,6 @@
 import post1 from "./posts/blog-post.20240105.md";
 import post2 from "./posts/blog-post.20231231.md";
+import post3 from "./posts/blog-post.20240111.md";
 import doraSummitFamilyPhoto from "./assets/dora-summit-family-photo-2023.png";
 import robDora from "./assets/robdoracommunity.jpg";
 
@@ -7,6 +8,18 @@ import robDora from "./assets/robdoracommunity.jpg";
 
 
 export const posts = [
+  {
+    title: "Bottom-up or top-down? DORA Community Discussion",
+    article: post3,
+    date: "2024-01",
+    dateText: "January 2024",
+    id: "2024-01-11-1",
+    tags: ["DORA", "Goals","Platform", "Cloud Costs", "Engineering Productivity", "AI", "Metrics"],
+    description: "Recap from the DORA Community Discussion on the focus for 2024.",
+    image: robDora,
+    imageLabel: "DORA Community Lean Coffee",
+    featured: true,
+  },
   {
     title: "January Community News",
     article: post1,
@@ -29,7 +42,7 @@ export const posts = [
     description: "Recap of 2023 in the DORA Community of Practice",
     image: doraSummitFamilyPhoto,
     imageLabel: "DORA Community Summit Family Photo",
-    featured: true,
+    featured: false,
   },
 ];
 
