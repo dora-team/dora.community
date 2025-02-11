47831bb830b9396dbb8e8ca2e9a56c8e5ec3e188
Nathen Harvey <nathenharvey@google.com>
2025-02-10 18:32:24 -0800

    Add playwright testing
    This adds some simple tests to the home page
    
    Addresses #108

--- test/playwright/tests/HomePage.spec.ts
+++ test/playwright/tests/HomePage.spec.ts
@@ -21,3 +21,35 @@ test('JOIN THE DORA COMMUNITY OF PRACTICE button opens correct link in new tab',
   await expect(newPage).toHaveURL('https://groups.google.com/g/dora-community/about');
 });
 
+test('DORA.dev card button navigates to correct URL', async ({ page }) => {
+  const [newPage] = await Promise.all([
+    page.waitForEvent('popup'),
+    page.getByRole('button', { name: 'Explore DORA.dev' }).click(),
+  ]);
+  await expect(newPage).toHaveURL('https://dora.dev');
+});
+
+test('T-shirts card button navigates to correct URL', async ({ page }) => {
+  const [newPage] = await Promise.all([
+    page.waitForEvent('popup'),
+    page.getByRole('button', { name: 'Browse t-shirts now' }).click(),
+  ]);
+  await expect(newPage).toHaveURL('https://dora.dev/merch/');
+});
+
+test('YouTube button navigates to correct URL', async ({ page }) => {
+  const [newPage] = await Promise.all([
+    page.waitForEvent('popup'),
+    page.getByRole('button', { name: 'Subscribe to Our Channel' }).click(),
+  ]);
+  await expect(newPage).toHaveURL('https://www.youtube.com/@dora-dev?sub_confirmation=1');
+});
+
+test('Homepage displays a calendar iframe', async ({ page }) => {
+  await expect(page.locator('iframe[src*="calendar.google.com"]')).toBeVisible();
+});
+
+test('DORA.dev card contains expected text', async ({page}) => {
+  await expect(page.getByText('DORA is the largest and longest running research program')).toBeVisible();
+});
+
