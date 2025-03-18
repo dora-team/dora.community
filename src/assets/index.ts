3f69cad908371d357bf312e6753d8c9750c0da55
Nathen Harvey <nathenharvey@google.com>
2025-03-18 17:02:45 -0400

    Clean-up the /join page
    * Add Dhruv as Guide and clean-up Join page
    * Add a second row for DORA Community Guides
      * Lisa and Dhruv are on the second row of Guides
    * Remove cards for community connections and community summit
    * Combine mailing list and lean coffee discussions on the same row
    * Update Nathen's bio
    * formatting updates
    * Cleans-up the imports
      * creates two new files that export the relevant components
        * `src/assets/index.ts`
        * `src/components/index.ts`
    * Adds tests for the Join page
    * Adds comment to the tests for the Home page about why one test is skipped

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+export { default as amandaLewisHeadshot } from './amandalewis-headshot.jpg';
+export { default as daveStanke } from './davestanke-headshot.png';
+export { default as denaliLumma } from './DenaliLumma.jpeg';
+export { default as dhruvAgarwal } from './DhruvAgarwal.jpg';
+export { default as lisaCrispin } from './lisa-crispin-headshot.jpg';
+export { default as nathenHarvey } from './nathen-harvey-3000.jpg';
+export { default as saulWilliamson } from './BetsalelWilliamson.jpeg';
+export { default as steveFenton } from './steve.jpg';
